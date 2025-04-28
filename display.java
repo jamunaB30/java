f7f6b9b80fb29fd1ff71716283def260a2e5817e
jamunaB30 <baskar97319@gmail.com>
2025-04-28 20:04:07 +0530

    display

---
+++ display.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+class display
+{
+  public static void main(String args[])
+  {
+   
+    System.out.println("Enter the number");
+    for(int i=0;i<=10;i++)
+    {
+       System.out.println(i);
+    }
+ }
+}
